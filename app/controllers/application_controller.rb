d519ce94a321e06053aed480d1bd63f7921209d7
Thomas Ebermann <plotti@gmx.net>
2016-10-06 12:37:22 +0200

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   private
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    if Rails.env.development?
+        @current_user = User.first
+    else
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
   end
   helper_method :current_user
 end
